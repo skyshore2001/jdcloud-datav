a65e2343589f887bbb72d33af177ddc6b4438bee
liangjian <liangjian@oliveche.com>
2021-06-28 18:19:30 +0800

    init demo. see callSvr in digitalFlop.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <datav/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import datav from './components/datav/index.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    datav
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import dataV from '@jiaminghi/data-view'
+
+Vue.use(dataV)
 
 Vue.config.productionTip = false
 
